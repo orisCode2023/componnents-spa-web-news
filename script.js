f2b41e89cc0c67cff033e7842a0deeee11743140
odmark2023@gmail.com <odmark2023@gmail.com>
2026-02-09 15:26:18 +0200

    finishing page 1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container')
-console.log(container)
+const mainPage = createHtmlElement('main')
+const dummyText = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia odio, fugit architecto molestiae repellat sed deleniti fuga rem debitis impedit. Commodi veritatis qui porro blanditiis, dolorum voluptates, eos voluptas, maxime soluta saepe voluptatum. Labore culpa qui suscipit deserunt fugiat laudantium mollitia itaque expedita, exercitationem recusandae! Voluptas optio porro eaque? Quidem?"
+
 
 function createHtmlElement(tag) {
     return document.createElement(tag)
@@ -39,38 +41,73 @@ function addNavBar() {
     createStoryA.href = '/index.js'
     btnDivNav.append(homePageBtn, createStoryBtn)
     homePageBtn.append(homePageA),
-    createStoryBtn.append(createStoryA),
-    container.append(nav)
+        createStoryBtn.append(createStoryA),
+        container.append(nav)
     nav.append(
         headLine,
         btnDivNav
     )
 }
 
-function addingMain(){
-    const main = createHtmlElement('main')
+function newsHeadLine() {
     const topStories = addElementWithTextContent('h2', 'Top Stories')
-    main.append(topStories)
-    container.append(main)
-    return main
+    mainPage.append(topStories)
 }
 
-function createNewsCard(authorName, headline, articalContent){
-    const cardDiv = addElementWithClass('div', 'cards')
+function cardsTamplate(cardsNumber) {
+    const divWrapper = addElementWithClass('div', 'cardsWrapper')
+    mainPage.append(divWrapper)
+    for (let index = 1; index <= cardsNumber; index++) {
+        const element = createNewsCard("Q Tarantino", "Once Upon A Time In Holliwood",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.", `card${index}`)
+        divWrapper.append(element)
+    }
+    // createNewsCard("K Tarantino", "Once Upon A Time In Holliwood", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.", 'card1')
+    // createNewsCard("Cary Joji Fukunaga", "No Time To Die", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.", 'card2')
+    // createNewsCard("J Cameron", "Titanik", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.", 'card3')
+    // createNewsCard("Disney", "The Lion King", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.", 'card4')
+}
+
+function createNewsCard(authorName, headline, articalContent, cardClass) {
+    const cardDiv = addElementWithClass('div', 'cards', cardClass)
+    const cardLink = addElementWithClass('a', 'card-link')
     const author = addElementWithTextContentAndClass('p', 'reporter-name', authorName)
     const title = addElementWithTextContentAndClass('h4', 'news-title', headline)
-    const artical = addElementWithTextContentAndClass('p', 'reporter-name', articalContent)
-    const main = addingMain()
-    main.append(cardDiv)
-    cardDiv.append(
+    const artical = addElementWithTextContentAndClass('p', 'artical-text', articalContent)
+    const img = addElementWithClass('img', 'card-img')
+    img.src = randomImg()
+    cardLink.href = './index.html'
+    cardDiv.append(cardLink)
+    cardLink.append(
+        title,
         author,
-        title, 
-        artical
+        artical,
+        img
     )
-    
+    return cardDiv
 }
-function main(){
+
+function randomImg(){
+    const imgUrl = "https://picsum.photos/200/300"
+    const qMark = '?'
+    const randomNumber = Math.floor(Math.random() * 100)
+    return imgUrl.concat(qMark,randomNumber)
+}
+
+
+function flashNews(){
+    const news = addElementWithClass('div', 'flash-news')
+    const img = addElementWithClass('img', 'img-news')
+    img.src = imgUrl
+    const headLine = addElementWithClass('h2', 'news-headline') 
+}
+
+function main() {
     addNavBar()
-    createNewsCard("tarantino", "once upon a time in holliwood", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nobis.")
+    container.append(mainPage)
+    newsHeadLine()
+    cardsTamplate(4)
+    // flashNews()
 }
+
 main()
